c4ca31327271c3f7a6bf1767ed356d577d6bd70e
Justica <atsuronreplaced@gmail.com>
2022-09-21 23:37:55 +0900

    Impl 2dplot

--- grapihx/gui/gui_gfx.py
+++ grapihx/gui/gui_gfx.py
@@ -67,11 +67,85 @@ class DynamicFrame(tk.Frame):
         return np.sin(x * freq)
 
 
+class DynamicFrame2D(tk.Frame):
+    INIT_VAL: float = 1.0
+    def __init__(self, master):
+        main_grid_param = {
+            "padx": 6,
+            "pady": 6
+        }
+        super(DynamicFrame2D, self).__init__(master)
+        self.master = master
+
+        frame = tk.Frame(self.master)
+
+        # main board
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(1, 1, 1)
+        self.fig_canvas = FigureCanvasTkAgg(self.fig, master=frame)
+
+        self.toolbar = NavigationToolbar2Tk(self.fig_canvas, frame)
+        self.fig_canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+        frame.grid(column=0, row=0, **main_grid_param)
+
+        # draw canvas
+        _arr = np.linspace(-np.pi, np.pi, 128)
+
+        self.x_mesh, self.y_mesh = np.meshgrid(_arr, _arr)
+        self.z_mesh = DynamicFrame2D.sincos(self.x_mesh, self.y_mesh, DynamicFrame2D.INIT_VAL, DynamicFrame2D.INIT_VAL)
+        self.ax_plot = self.ax.pcolor(self.x_mesh, self.y_mesh, self.z_mesh)
+        self.fig_canvas.draw()
+
+        # sidebar
+        self.side_bar = tk.Frame(self.master)
+        self.side_bar.grid(column=1, row=0, **main_grid_param)
+
+        # button
+        button = tk.Button(self.side_bar, text="Reset", command=self.reset)
+        button.pack()
+
+        # slider
+        self.val_s = tk.DoubleVar()
+        self.val_s.set(DynamicFrame2D.INIT_VAL)
+        self.sli_s = tk.Scale(
+            self.side_bar, variable=self.val_s, orient=tk.HORIZONTAL, length=300, resolution=0.2,
+            from_=-5, to=5, command=self.on_slide_s
+        )
+        self.sli_s.pack()
+
+        self.val_c = tk.DoubleVar()
+        self.val_c.set(DynamicFrame2D.INIT_VAL)
+        self.sli_c = tk.Scale(
+            self.side_bar, variable=self.val_c, orient=tk.HORIZONTAL, length=300, resolution=0.2,
+            from_=-5, to=5, command=self.on_slide_c
+        )
+        self.sli_c.pack()
+
+    def reset(self):
+        self.val_s.set(DynamicFrame2D.INIT_VAL)
+        self.draw(DynamicFrame2D.INIT_VAL, DynamicFrame2D.INIT_VAL)
+
+    def on_slide_s(self, e):
+        self.draw(float(e), self.val_c.get())
+
+    def on_slide_c(self, e):
+        self.draw(self.val_s.get(), float(e))
+
+    def draw(self, *args):
+        self.z_mesh = DynamicFrame2D.sincos(self.x_mesh, self.y_mesh, *args)
+        self.ax_plot.set_array(np.ma.core.MaskedArray(self.z_mesh))
+        self.fig_canvas.draw()
+
+    @staticmethod
+    def sincos(x, y, freq_s, freq_c) -> np.ndarray:
+        return np.sin(x * freq_s) * np.cos(y * freq_c)
+
+
 class GuiGfx(BaseGfx):
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("Fitter")
-        self.dynamic_frame = DynamicFrame(self.root)
+        self.dynamic_frame = DynamicFrame2D(self.root)
         self.root.protocol("WM_DELETE_WINDOW", self.end)
 
     def start(self):
